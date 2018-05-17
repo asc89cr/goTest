2df10c4ea3fc59577dd86683e21b8ef0c2549d51
Sandi, ArnoldX <arnoldx.sandi@intel.com>
2018-05-17 12:25:19 -0600

    Testing

--- hello.go
+++ hello.go
@@ -1,10 +1,20 @@
 package main
 
-import( 
+import (
 	"fmt"
 	"stringutil"
-	)
+)
 
-func main(){
+func main() {
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+	fmt.Println(int64(d))
+
+	for j := 1; j <= 10; j++ {
+		fmt.Println(j)
+	}
+
 }
